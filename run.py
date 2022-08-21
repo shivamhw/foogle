b19571ec20d3451112529346082229dc2661ecd8
shivamhw <s.kmishra703@gmail.com>
2022-08-21 21:07:49 +0530

    added pymongo

--- run.py
+++ run.py
@@ -8,6 +8,7 @@ cfg["CF_WORKER_SITE"] = config["foogle"]["CF_WORKER_SITE"]
 cfg["CRED_JSON_PATH"] = config["foogle"]["CRED_JSON_PATH"]
 cfg["TOKEN_JSON_PATH"] = config["foogle"]["TOKEN_JSON_PATH"]
 cfg["TEMP_FOLDER"] = config["foogle"]["TEMP_FOLDER"]
+cfg["MONGOURI"] = config["foogle"]["MONGOURI"]
 if __name__ == "__main__":
     create_app(**cfg).run(host="0.0.0.0", port="5500",debug=True)
 else:
